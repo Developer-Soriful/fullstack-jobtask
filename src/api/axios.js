61e4a27b5f5bd50e2884255c12e08000d8f63934
Developer-Soriful <93891396soriful@gamil.com>
2025-09-08 13:45:26 +0600

    added live backend deployed link

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api"; // Backend URL
+const API_URL = "https://fullstack-jobtask-backend.onrender.com/api"; // Backend URL
 
 const axiosInstance = axios.create({
-  baseURL: API_URL,
+    baseURL: API_URL,
 });
 
 // Add JWT token automatically for protected routes
 axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 });
 
 export default axiosInstance;
